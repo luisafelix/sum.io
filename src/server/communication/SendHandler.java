4eb6044497c8bb6127e4e342b21a19d4502a21ba
Jorge Korgut <jorgekorgut@hotmail.com>
2021-03-13 19:34:59 +0100

    seting up the project

---
+++ src/server/communication/SendHandler.java
@@ -0,0 +1,43 @@
+package server.communication;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import common.communication.SyncPack;
+
+public class SendHandler{
+	
+	private Socket socket;
+	private ObjectOutputStream toClient;
+	
+	public SendHandler(Socket socket){
+		this.socket = socket;
+		try{
+			toClient = new ObjectOutputStream(socket.getOutputStream());
+		}catch(Exception e){
+			//e.printStackTrace();
+		}
+	}
+	
+	//TODO: Some updates to the client in a determinated time.
+	public void send(SyncPack sPack){
+		try{
+			toClient.writeObject(sPack);
+			toClient.reset(); // do not forget the reset, lose data from array list. Why ???
+		}catch(Exception e){
+		}
+
+	}
+	
+	public void close(){
+		try{
+			if(socket !=null){
+				socket.close();
+			}
+		}catch(Exception e){
+			
+		}
+	}
+	
+}
---
+++ src/server/engine/ScreenRender.java
@@ -0,0 +1,107 @@
+package server.engine;
+
+//Question: Is it good pratice to declare with '*' instead of the name of each class ?
+import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Hashtable;
+
+import javax.swing.JPanel;
+
+import common.environment.GameObject;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import java.io.File;
+import javax.imageio.ImageIO;
+
+public class ScreenRender extends JPanel{
+	
+	private PriorityQueue<GameObject> renderingQueue;
+	
+	//FIXME: try to find something better in the future.
+	private Hashtable<String,BufferedImage> imageMap = null;
+	
+	public ScreenRender()
+	{
+		renderingQueue = new PriorityQueue<GameObject>();
+		imageMap = new Hashtable<String,BufferedImage>();
+		loadImages();
+	}
+	
+	private void loadImages()
+	{
+		//Find all the images names in the res folder
+		File res = new File(System.getProperty("user.dir") + System.getProperty("file.separator")+"res");
+		String[] imageNames = res.list();
+		try{
+			for(int i = 0;i < imageNames.length;i++)
+			{
+				//just loads image that are in png format.
+				if(imageNames[i].endsWith(".png"))
+				{
+					BufferedImage image = ImageIO.read(new File(res + System.getProperty("file.separator") + imageNames[i]));
+					imageMap.put(imageNames[i].substring(0,imageNames[i].length()-4),image);
+				}
+			}
+		} catch(Exception e) {
+			System.out.println("Error: Image did not load");
+		}	
+	}
+	
+	public void addToRender(GameObject go)
+	{
+		if(!renderingQueue.contains(go))
+		{
+			renderingQueue.add(go);
+		}
+	}
+	
+	public void addToRender(ArrayList<GameObject> goList)
+	{
+		for(GameObject go : goList )
+		{
+			addToRender(go);
+		}
+	}
+	
+	public boolean removeToRender(GameObject go)
+	{
+		if(renderingQueue.remove(go))
+		{
+			return true;
+		}
+		System.out.println("Error: Rendering queue does not contains that object");	
+		return false;
+	}
+	
+	@Override
+	public String toString()
+	{
+		String res = "";
+		res += "Size: " + renderingQueue.size() + "\n";
+		for(GameObject go : renderingQueue)
+		{
+			res += go.toString();
+		}
+		return res;
+	}
+	
+	//ADDLATER: Double Buffering.
+	@Override
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		for(GameObject go:renderingQueue)
+		{
+			BufferedImage currentImage = imageMap.get(go.getName());
+			if(currentImage == null)
+			{
+				currentImage = imageMap.get("noTexture");
+			}
+			g.drawImage(currentImage,this.getWidth()/2 + go.getX()-go.getWidth()/2,this.getHeight()/2 + go.getY()-go.getHeight()/2,go.getWidth(),go.getHeight(),null);
+		}
+	}
+}
+
